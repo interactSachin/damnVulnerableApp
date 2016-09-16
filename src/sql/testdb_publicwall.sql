7315f3c89cd8dd5b9191e27c3fe587e9e3ea829f
interactSachin <interactsachin@gmail.com>
2016-09-16 19:48:36 +0530

    base sql
    contains db tables

---
+++ src/sql/testdb_publicwall.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `testdb`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	5.7.14-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `publicwall`
+--
+
+DROP TABLE IF EXISTS `publicwall`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publicwall` (
+  `comment` varchar(512) NOT NULL,
+  `cmtId` int(16) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(32) NOT NULL,
+  PRIMARY KEY (`cmtId`)
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publicwall`
+--
+
+LOCK TABLES `publicwall` WRITE;
+/*!40000 ALTER TABLE `publicwall` DISABLE KEYS */;
+INSERT INTO `publicwall` VALUES ('This is how a comment is displayed.',1,'1'),('<Your Comment here>',2,'2'),('1234567890!@#$%^&*()<br><br><br><br><br><br><br><br><br><br><br><br><br>',47,'1'),('Please Don\'t use<h5>script</h5>tag<br><br><br><br><br><br><br><br><br><br><br><br><br>',48,'1'),('Application will auto clean the comment section after a threshhold',51,'1'),('<img src=\"\"\"/>',63,'4'),('<img src=\"\"/>',64,'4'),('<img src=\"this is eimage\">test</img>',65,'4');
+/*!40000 ALTER TABLE `publicwall` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-16 19:45:01
